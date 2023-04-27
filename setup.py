5958844faa4e6ef020522d74386e43a8868bd40b
Nan-Zhu <243504466@qq.com>
2023-04-27 20:21:38 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='openalextool',
+      version='0.0.1',
+      description='Open Alex utilities',
+      maintainer='Nan Zhu',
+      maintainer_email='nanzhu@andrew.cmu.edu',
+      license='GPL',
+      packages=['openalextool'],
+      scripts=[],
+      long_description='''A set of Open Alex utilities''')
